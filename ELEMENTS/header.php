f94b157e3a8baf48af235c6f4bae4d7bc0833ebb
Doan Tien Thanh <thanh.doan21@pn.com.vn>
2020-12-05 09:06:18 +0700

    update

--- ELEMENTS/header.php
+++ ELEMENTS/header.php
@@ -12,16 +12,16 @@
         </div>
     </div>
     <div class="main-header">
-<div class="col-3 col-s-3 left-main-header">
-<img src="../IMAGE/logo.png" class="image-logo" alt="tienthanhshop">
-</div>
-<div class="col-8 col-s-8 center-main-header">
-</div>
-<div class="col-2 col-s-2 right-main-header">
-   <div class="quantity-customer-cart">0</div>
-<span><i class='fas fa-shopping-basket customer-cart'></i></span>
-<span class="price-customer-carts">20000000đ</span>
-</div>
+        <div class="col-3 col-s-3 left-main-header">
+            <img src="../IMAGE/logo.png" class="image-logo" alt="tienthanhshop">
+        </div>
+        <div class="col-8 col-s-8 center-main-header">
+        </div>
+        <div class="col-2 col-s-2 right-main-header">
+            <div class="quantity-customer-cart">0</div>
+            <span><i class='fas fa-shopping-basket customer-cart'></i></span>
+            <span class="price-customer-carts">20000000đ</span>
+        </div>
     </div>
     <div class="menu-header">
         menu
